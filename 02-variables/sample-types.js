61cd99a26b3484ebb47b17712b84e9707e5574ac
nibchee <nirbhaybansal28@gmail.com>
2023-06-18 03:42:25 +0530

    variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Nirbhay";
+var lastName = "Bansal";
+console.log(found);
+console.log("The grad is " + grade);
+console.log("Hi" + firstName + " " + lastName);
+//use template Strings
+console.log("Hi ".concat(firstName));
