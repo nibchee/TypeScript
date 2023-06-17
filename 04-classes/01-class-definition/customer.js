78a7cfbb0f9bbe5992693c71d6b39ee94c8f8dec
nibchee <nirbhaybansal28@gmail.com>
2023-06-18 04:14:12 +0530

    Creating class

---
+++ 04-classes/01-class-definition/customer.js
@@ -0,0 +1,11 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+//lets create an insatnce
+var myCustomer = new Customer();
+myCustomer.firstName = "Nirbhay";
+myCustomer.lastName = "Bansal";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/customer.ts
@@ -0,0 +1,14 @@
+class Customer{
+    firstName:string;
+    lastName:string;
+
+}
+
+//lets create an insatnce
+let myCustomer=new Customer();
+
+myCustomer.firstName="Nirbhay";
+myCustomer.lastName="Bansal";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
