8bc2fc89e602e6324d1b9da7a3c1ce29c9b39ed2
nibchee <nirbhaybansal28@gmail.com>
2023-06-18 16:34:52 +0530

    Modules import export

---
+++ 04-classes/05-modules/driver.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const customer_1 = require("./customer");
+//lets create an insatnce
+let myCustomer1 = new customer_1.Customer1("Nirbhay", "Bansal");
+//myCustomer.firstName="Nirbhay";
+//myCustomer.lastName="Bansal";
+console.log(myCustomer1.firstName);
+console.log(myCustomer1.lastName);
---
+++ 04-classes/05-modules/driver.ts
@@ -0,0 +1,9 @@
+import { Customer1 } from "./customer";
+//lets create an insatnce
+let myCustomer1=new Customer1("Nirbhay","Bansal");
+
+//myCustomer.firstName="Nirbhay";
+//myCustomer.lastName="Bansal";
+
+console.log(myCustomer1.firstName);
+console.log(myCustomer1.lastName);
